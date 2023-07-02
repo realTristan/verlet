64fd4d78350e41dd094d0caa097d96d339002597
Tristan <heytristaann@gmail.com>
2023-07-02 00:09:34 -0400

    todo: fix friction

--- verlet/ball/constraints/circle.py
+++ verlet/ball/constraints/circle.py
@@ -22,14 +22,14 @@ class VerletBallCircleConstraint:
         dist: list[float] = [self.position[0] - verlet_ball.pos_cur[0],
                              self.position[1] - verlet_ball.pos_cur[1]]
 
-        # the vector magnitude of the ball
+        # The vector magnitude of the ball
         mag: float = (dist[0] ** 2 + dist[1] ** 2) ** 0.5
-        delta: float = self.radius - verlet_ball.radius  # if the constraint is a circle
+        delta: float = self.radius - verlet_ball.radius
         if mag > delta:
             verlet_ball.pos_cur[0] = self.position[0] - dist[0] / mag * delta
             verlet_ball.pos_cur[1] = self.position[1] - dist[1] / mag * delta
 
             # Update the verlet ball velocity to account for friction
-            if verlet_ball.friction is not None:
-                verlet_ball.velocity[0] -= verlet_ball.friction.__getitem__() * \
-                    dist[0] / mag * delta
+            # if verlet_ball.friction is not None:
+            #     verlet_ball.velocity[0] -= verlet_ball.friction.__getitem__() * \
+            #         dist[0] / mag * delta
