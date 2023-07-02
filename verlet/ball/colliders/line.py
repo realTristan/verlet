39b4f37be2481fe7de340cd0e9eafaa6db7df170
Tristan <heytristaann@gmail.com>
2023-07-02 18:39:45 -0400

    rename variables

--- verlet/ball/colliders/line.py
+++ verlet/ball/colliders/line.py
@@ -6,36 +6,36 @@ from physics import Vector2D
 class VerletBallLineCollider(object):
     def __init__(
         self, 
-        position: tuple[float, float], 
-        to: tuple[float, float], 
+        start: tuple[float, float], 
+        end: tuple[float, float], 
         width: int = 5,
         slope_multiplier: float = 0.06
     ) -> None:
-        self.position: Vector2D = Vector2D(position[0], position[1])
-        self.to: Vector2D = Vector2D(to[0], to[1])
+        self.start: Vector2D = Vector2D(start[0], start[1])
+        self.end: Vector2D = Vector2D(end[0], end[1])
         self.width: int = width
         self.color: tuple[int, int, int] = (255, 255, 255)
         self.slope_multiplier: float = slope_multiplier
 
     # Draw the Collider
     def draw(self, screen: pygame.Surface) -> None:
-        pygame.draw.line(screen, self.color, self.position.get(), 
-                         self.to.get(), self.width)
+        pygame.draw.line(screen, self.color, self.start.get(), 
+                         self.end.get(), self.width)
 
     # Apply the Collider
     def apply(self, vball: VerletBall) -> None:
-        l_bound: float = self.position.x - vball.radius
-        r_bound: float = self.to.x + vball.radius
+        l_bound: float = self.start.x - vball.radius
+        r_bound: float = self.end.x + vball.radius
         
         # If the circle is past the lines bounds
         if vball.current_position.x < l_bound or vball.current_position.x > r_bound:
             return
         
         # Calculate the slope of the line
-        m: float = (self.position.y - self.to.y) / (self.position.x - self.to.x)
+        m: float = (self.start.y - self.end.y) / (self.start.x - self.end.x)
 
         # Calculate the y-intercept of the line
-        y_int: float = self.position.y - m * self.position.x
+        y_int: float = self.start.y - m * self.start.x
 
         # Calculate the y position of the ball
         y: float = m * vball.current_position.x + y_int
