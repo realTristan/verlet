571b58d8faa5bd6d8437e20022ba0a1e40dbaaf4
Tristan <heytristaann@gmail.com>
2023-07-02 22:02:26 -0400

    daemon = true

--- utils/threads.py
+++ utils/threads.py
@@ -10,7 +10,7 @@ class Threads:
     # Run the thread
     def run(self, target, args: tuple):
         self.lock.acquire()
-        t = threading.Thread(target=target, args=args)
+        t = threading.Thread(target=target, args=args, daemon=True)
         t.start()
         t.join()
         self.lock.release()
