368c2663c68b3f7921c1fc6c220ba4be82ba4420
tristan <heytristaann@gmail.com>
2023-07-04 17:33:02 -0400

    rename

---
+++ objects/verlet/ball/colliders/borders/screen.py
@@ -0,0 +1,5 @@
+
+
+class Screen(object):
+    def __init__(self):
+        pass
--- objects/verlet/ball/colliders/bounds/screen.py
+++
@@ -1,8 +0,0 @@
-
-
-
-class RectBounds(object):
-    def __init__(self):
-        pass
-        # Make the screen width and height the bounds.
-        # The ball will bounce of the edges of the screen
